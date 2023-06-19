6d75f2c5f7f03a497aa55912e5e9299056741617
Po-Cheng Yeh <blp100x@hotmail.com>
2023-06-19 17:58:46 +0800

    feat: setup

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div className="m-auto my-10 block w-fit rounded-md bg-stone-300 p-4 text-4xl text-slate-600">
+      Hello
+    </div>
+  );
+};
+
+export default Page;
